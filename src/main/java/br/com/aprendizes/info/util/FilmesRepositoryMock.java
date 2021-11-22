e163ad9b22fe937e3b5123ffcf6008ff4654545f
mikaelsl <mikael2484>
2021-11-22 14:38:51 -0300

    alterações do filmes

--- src/main/java/br/com/aprendizes/info/util/FilmesRepositoryMock.java
+++ src/main/java/br/com/aprendizes/info/util/FilmesRepositoryMock.java
@@ -10,14 +10,16 @@ public class FilmesRepositoryMock {
     public static List<Filme> mockFilmesProvedorResponse(){
         List<Filme> filmeList = new ArrayList<>();
 
-        Filme duna = new Filme();
-        duna.setIdPessoa(1);
-        duna.setTitulo("Duna");
-        duna.setDiretor("Denis Vileneve");
-        duna.setDuracao("2H,28");
-        duna.setData_lancamento("21/10/2021");
-        duna.setElenco("Chani, Paul Atreides, Jessica Atreides, Duncan Idaho");
-        duna.setStreaming("HBOMAX");
+        Filme truqueDeMestre = new Filme();
+        truqueDeMestre.setIdPessoa(1);
+        truqueDeMestre.setTitulo("truque de Mestre");
+        truqueDeMestre.setDiretor("Denis Vileneve");
+        truqueDeMestre.setDuracao("2H,28");
+        truqueDeMestre.setData_lancamento("21/10/2021");
+        truqueDeMestre.setElenco("Chani, Paul Atreides, Jessica Atreides, Duncan Idaho");
+        truqueDeMestre.setStreaming("HBOMAX");
+
+        filmeList.add(truqueDeMestre);
 
         Filme spiderverse = new Filme();
         spiderverse.setIdPessoa(1);
@@ -28,19 +30,18 @@ public class FilmesRepositoryMock {
         spiderverse.setElenco("Miles Morales, Homem-Aranha,Spider-Women,Gatuno, Rei do Crime,");
         spiderverse.setStreaming("Telecine");
 
-        filmeList.add(duna);
         filmeList.add(spiderverse);
 
-        Filme jogosvorazes = new Filme();
-        jogosvorazes.setIdPessoa(2);
-        jogosvorazes.setTitulo("Jogos Vorazes");
-        jogosvorazes.setDiretor("Gary Ross");
-        jogosvorazes.setDuracao("2H,22");
-        jogosvorazes.setData_lancamento("22/03/2012");
-        jogosvorazes.setElenco("Katniss Everdeen, Gale Hawthome, peeta Mellark");
-        jogosvorazes.setStreaming("Netflix e Amazon Prime");
+        Filme chappie = new Filme();
+        chappie.setIdPessoa(2);
+        chappie.setTitulo("Chappie");
+        chappie.setDiretor("Gary Ross");
+        chappie.setDuracao("2H,22");
+        chappie.setData_lancamento("22/03/2012");
+        chappie.setElenco("Katniss Everdeen, Gale Hawthome, peeta Mellark");
+        chappie.setStreaming("Netflix e Amazon Prime");
 
-        filmeList.add(jogosvorazes);
+        filmeList.add(chappie);
 
         return  filmeList;
     }
