e5489cbbda418b2b6aea79295bf16c94a5ba2247
smachado <stiverson.machado@gmail.com>
2021-11-24 09:46:28 -0300

    correção na exibição de filmes da pessoa id 2 .

--- src/main/java/br/com/aprendizes/info/util/FilmesRepositoryMock.java
+++ src/main/java/br/com/aprendizes/info/util/FilmesRepositoryMock.java
@@ -43,6 +43,30 @@ public class FilmesRepositoryMock {
 
         filmeList.add(chappie);
 
+
+        Filme jogosvorazes = new Filme();
+        jogosvorazes.setIdPessoa(2);
+        jogosvorazes.setTitulo("Jogos Vorazes");
+        jogosvorazes.setDiretor("Gary Ross");
+        jogosvorazes.setDuracao("2H,22");
+        jogosvorazes.setData_lancamento("22/03/2012");
+        jogosvorazes.setElenco(" Jennifer Lawrence; Josh Hutcherson; Liam Hemsworth");
+        jogosvorazes.setStreaming("Netflix e Amazon Prime");
+
+        filmeList.add(jogosvorazes);
+
+
+        Filme alerta_vermelho = new Filme();
+        alerta_vermelho.setIdPessoa(2);
+        alerta_vermelho.setTitulo("Alerta Vermelho");
+        alerta_vermelho.setDiretor("Rawson Marshall Thurber");
+        alerta_vermelho.setDuracao("1h,55");
+        alerta_vermelho.setData_lancamento("12/11/2021");
+        alerta_vermelho.setElenco("Gal Gadot, Dwayne Johnson, Ryan Renolds");
+        alerta_vermelho.setStreaming("Netflix e Amazon Prime");
+
+        filmeList.add(alerta_vermelho);
+
         return  filmeList;
     }
 
